9e712f907f9038928f6bbcba0abfb2ee67dba53a
KSY <zan05134@naver.com>
2022-11-23 00:53:08 +0000

    incomplete lstm2

--- dkt/src/model.py
+++ dkt/src/model.py
@@ -70,6 +70,71 @@ class LSTM(nn.Module):
         return out
 
 
+class LSTM2(nn.Module):
+    def __init__(self, args):
+        super(LSTM2, self).__init__()
+        self.args = args
+
+        self.hidden_dim = self.args.hidden_dim
+        self.n_layers = self.args.n_layers
+
+        # Embedding
+        # interaction은 현재 correct로 구성되어있다. correct(1, 2) + padding(0)
+        self.embedding_correct = nn.Embedding(4, self.hidden_dim)
+        self.embedding_test = nn.Embedding(self.args.n_test + 1, self.hidden_dim // 3)
+        self.embedding_question = nn.Embedding(
+            self.args.n_questions + 1, self.hidden_dim // 3
+        )
+        self.embedding_tag = nn.Embedding(self.args.n_tag + 1, self.hidden_dim // 3)
+
+        # embedding combination projection
+        self.comb_proj = nn.Linear((self.hidden_dim // 3) * 3, self.hidden_dim)
+
+        self.lstm = nn.LSTM(
+            self.hidden_dim, self.hidden_dim, self.n_layers, batch_first=True
+        )
+
+        # Fully connected layer
+        self.fc = nn.Linear(self.hidden_dim, 1)
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, input):
+
+        test, question, tag, correct, _, interaction = input
+
+        batch_size = interaction.size(0)
+
+        # Embedding
+        embed_correct = self.embedding_correct(interaction)
+        embed_test = self.embedding_test(test)
+        embed_question = self.embedding_question(question)
+        embed_tag = self.embedding_tag(tag)
+
+        embed = torch.cat(
+            [
+                embed_test,
+                embed_question,
+                embed_tag,
+            ],
+            2,
+        )
+
+        test_X = self.comb_proj(embed)
+
+        X = torch.mul(test_X, embed_correct)
+        #X = torch.sum(embed, 1)
+        # X : (batch * max_seq_len * hidden_dim)
+        # out : (batch * max_seq_len * hidden_dim)
+        out, _ = self.lstm(X)
+        #out = out.contiguous().view(batch_size, -1, self.hidden_dim)
+        #out = self.relu1(out)
+        out = self.fc(out).view(batch_size, -1) # (batch_size, hidden_dim)
+        out = self.softmax(out)
+        return out
+
+
+
+
 class LSTMATTN(nn.Module):
     def __init__(self, args):
         super(LSTMATTN, self).__init__()
